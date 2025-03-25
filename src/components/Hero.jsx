c2f2740eed97ebd71ebac795080f487b251654b2
Brandon Vazquez <bvazquezj1700@gmail.com>
2025-03-24 21:51:21 -0600

    feature: add button for download cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,11 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import { MdOutlineAlternateEmail } from "react-icons/md";
 // import Spline from '@splinetool/react-spline';
 
+function handleDownloadcv() {
+  window.open('https://drive.usercontent.google.com/u/0/uc?id=1J5jkjEdDZTXXaMAv6qig1UT7T9EumE5d&export=download',
+    '_blank');
+}
+
 const Hero = () => {
   return (
     <section id="inicio" className="h-screen bg-hero-pattern flex items-center justify-evenly bg-gray-100">
@@ -22,19 +27,22 @@ const Hero = () => {
             transition={{ duration: 0.8, delay: 0.2 }}
             className="text-zinc-600 text-base mb-4"
           >
-            Estudiante de ultimo año de Ingeniería en Informática, <br></br>
+            Estudiante de ultimo semestre de Ingeniería en Informática, <br></br>
             Desarrollador Ful-Stack | Ingeniero de Software.
           </motion.p>
-          <motion.button
-            to="proyectos"
-            initial={{ opacity: 0, y: 0, delay: 0.7 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.7 }}
-            className=" bg-red-800 text-lg text-white w-32 px-5 py-3 rounded-3xl hover:bg-red-950 transition duration-300"
-            onClick={() => { document.getElementById('proyectos').scrollIntoView({ behavior: 'smooth' }) }}
-          >
-            Proyectos
-          </motion.button>
+          <div className='flex gap-5 items-center justify-center'>
+            <motion.button
+              onClick={handleDownloadcv}
+              initial={{ opacity: 0, y: 0, delay: 0.7 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.7 }}
+              className="flex items-center justify-center gap-1 bg-red-800 text-sm text-white px-4 py-2 rounded-3xl hover:bg-red-950 hover:text-white transition duration-300"
+            >
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+              Descargar CV
+            </motion.button>
+          </div>
         </div>
         {/* <div className='min-w-96 aspect-square'>
           <Spline
