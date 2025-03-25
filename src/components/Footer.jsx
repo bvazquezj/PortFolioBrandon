ff3773d64df9ce8f33f6b08a09ced6cb74ecb9fa
Brandon Vazquez <bvazquezj1700@gmail.com>
2025-03-24 21:11:04 -0600

    fixed: change on footer and better presentation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,79 @@
 
 import { motion } from 'framer-motion';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-scroll';
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            className="mb-4 md:mb-0"
+      <div className="flex flex-col md:flex-row justify-between items-center">
+        <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="mb-4 md:mb-0 text-center md:text-left"
+        >
+        <p className="text-sm">&copy; 2025 Mi portafolio.</p>
+        <div className="flex flex-col md:flex-row md:space-x-4 mt-2">
+          <Link
+          to="inicio"
+          className="text-gray-400 hover:text-white transition duration-300 cursor-pointer"
           >
-            <p>&copy; 2023 My Portfolio. All rights reserved.</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-            className="flex space-x-4"
+          Inicio
+          </Link>
+          <Link
+          to="acerca de"
+          className="text-gray-400 hover:text-white transition duration-300 cursor-pointer"
           >
-            <a href="#" className="hover:text-gray-400 transition duration-300">
-              <FaGithub size={24} />
-            </a>
-            <a href="#" className="hover:text-gray-400 transition duration-300">
-              <FaLinkedin size={24} />
-            </a>
-            <a href="#" className="hover:text-gray-400 transition duration-300">
-              <FaTwitter size={24} />
-            </a>
-          </motion.div>
+          Acerca de
+          </Link>
+          <Link
+          to="proyectos"
+          className="text-gray-400 hover:text-white transition duration-300 cursor-pointer"
+          >
+          Proyectos
+          </Link>
+          <Link
+          to="contacto"
+          className="text-gray-400 hover:text-white transition duration-300 cursor-pointer"
+          >
+          Contacto
+          </Link>
         </div>
+        </motion.div>
+        <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="flex space-x-4"
+        >
+        <a
+          href="https://github.com/bvazquezj"
+          className="text-gray-400 hover:text-white transition duration-300"
+          target="_blank"
+        >
+          <FaGithub size={24} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/bvazquezj/"
+          className="text-gray-400 hover:text-white transition duration-300"
+          target='_blank'
+        >
+          <FaLinkedin size={24} />
+        </a>
+        <a
+          href="https://x.com/bvazquezj"
+          className="text-gray-400 hover:text-white transition duration-300"
+          target='_blank'
+        >
+          <FaTwitter size={24} />
+        </a>
+        </motion.div>
+      </div>
       </div>
     </footer>
-  );
+    );
 };
 
 export default Footer;
